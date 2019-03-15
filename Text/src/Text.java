c5f6d8575d6c6aac28ce9908ba6d2c395698b025
parkwoongki <sbe03005@naver.com>
2019-03-15 22:04:19 +0900

    Lecture Take Notes

---
+++ Text/src/Text.java
@@ -0,0 +1,24 @@
+public class Text {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * println 안에 자료형에 따라 오버로딩
+		 * static은 메모리에 올려놓고 실행
+		 * static은 public
+		 * this는 클래스 자체
+		 * super() 이거는 부모를 호출한다고 말해주고 객체 오브젝트를 말해줌
+		 * static 정적 멤버는 println 처럼 그냥 갔다쓰는거 임
+		 * static은 프로그램 실행되면 바로 실행, 일반 변수와 선언되는 메모리 위치가 다름, 가비지 콜랙터에 의해 사라지지 않음
+		 * static으로 선언해 놓으면 객체를 생성하지 않고 그냥 사용가능, 공유로도 사용하지만 상수로도 사용한다.
+		 * static value 값은 상수로 들어감 혹은 static final로 선언을 해논다.
+		 * util은 항상 같은 값을 리턴하는 것
+		 * 간단히 하자면 static은 객체를 안만들고 사용할 수 있는 것이다.
+		 * @override 같은거 어노테이션
+		 * 
+		 * 상속은 재사용의 목적이 크다.
+		 * 
+		 */
+	}
+
+}
