6471641c788913df1b8a9bef1cacaba6e9b3b50e
parkwoongki <sbe03005@naver.com>
2019-03-20 12:06:53 +0900

    Exception Practice by Bank System

---
+++ NewException/src/BalInsException/Account.java
@@ -0,0 +1,23 @@
+package BalInsException;
+
+public class Account {
+	private long balance;
+
+	public Account() {
+	}
+
+	public long getBalance() {
+		return balance;
+	}
+
+	public void deposit(int money) {
+		balance += money;
+	}
+
+	public void withdraw(int money) throws BalInsException {
+		if (balance < money) {
+			throw new BalInsException("잔고부족 : " + (money - balance) + " 모자람");
+		}
+		balance -= money;
+	}
+}
---
+++ NewException/src/BalInsException/BalInsException.java
@@ -0,0 +1,10 @@
+package BalInsException;
+
+public class BalInsException extends Exception {
+	public BalInsException() {
+	}
+
+	public BalInsException(String message) {
+		super(message);
+	}
+}
