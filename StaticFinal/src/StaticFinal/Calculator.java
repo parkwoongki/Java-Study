279f933ddf23a6cd837e711a06b481da2ac4bb70
parkwoongki <sbe03005@naver.com>
2019-03-21 01:39:51 +0900

    Static Final

---
+++ StaticFinal/src/StaticFinal/Calculator.java
@@ -0,0 +1,13 @@
+package StaticFinal;
+
+public class Calculator {
+	public static final double PI_MATH = 3.14159;
+
+	public static int plus (int x, int y){
+		return x+y;
+	}
+	
+	public static int minus (int x, int y){
+		return x-y;
+	}
+}
